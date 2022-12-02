fd7d6efedba4334a52b4cd85e1526d9380bb8b9f
vanessaBonito <vanebonito@hotmail.com>
2022-12-01 21:26:04 -0500

    Add component Greeting

---
+++ app/javascript/components/Greeting.js
@@ -0,0 +1,9 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+const Greeting = () => {
+
+    return <h1>Hello</h1>
+};
+
+export default Greeting;
