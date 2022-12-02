68d41dba36d5a782019982b98c4d6fd65e248bf1
vanessaBonito <vanebonito@hotmail.com>
2022-12-02 08:28:37 -0500

    Display the fetch greeting

--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,9 +1,15 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { getGreeting } from '../redux/greetings/greetings';
 
 const Greeting = () => {
+    const dispatch = useDispatch();
+    const greeting = useSelector((state) => state.message.message);
+    useEffect(() => {
+        dispatch(getGreeting());
+    }, [dispatch]);
 
-    return <h1>Hello</h1>
+    return <h1>{greeting}</h1>
 };
 
 export default Greeting;
