2f841e792c3743f351778ebf122020c474d2f926
vanessaBonito <vanebonito@hotmail.com>
2022-12-02 08:05:47 -0500

    Add reducer greetings

---
+++ app/javascript/redux/greetings.js
@@ -0,0 +1,38 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+// First, create the thunk
+export const getGreeting = createAsyncThunk(
+  'greetings/getGreeting',
+  async () => {
+    const response = await fetch('http://localhost:3000/greetings/index');
+    return response.json()
+  },
+);
+
+const initialState = {
+    message: [],
+    status: 'idle',
+};
+
+// Then, handle actions in your reducers:
+const reducerGreetings = createSlice({
+  name: 'greetings',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    // Add reducers for additional action types here, and handle loading state as needed
+    builder.addCase(getGreeting.fulfilled, (state, action) => {
+        state.message = action.payload;
+        state.status = 'succeeded';
+    });
+    builder.addCase(getGreeting.pending, (state, action) => {
+        state.message = action.payload;
+        state.status = 'loading';
+    });
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const { greetingsReducer } = reducerGreetings.actions;
+
+export default reducerGreetings.reducer;
